cebadd3a511ea2cf90a3b8144902fcf27ab5289a
heykkyu <heykkyu@gmail.com>
2020-03-24 23:19:15 +0900

    resume start

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,38 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   
+<!--Header-->
+  <header class="main-header" style="background-image: url(assets/img/img_bg_header.jpg)">
+    <div class="container">
+      <div class="row personal-profile">
+        <div class="col-md-4 personal-profile__avatar">
+          <img class="" src="" alt="avatar">
+        </div>
+        <div class="col-md-8">
+          <p class="personal-profile__name">Jeongkyu Kim</p>
+          <p class="personal-profile__work">Frontend Developer</p>
+          <div class="personal-profile__contacts">
+            <dl class="contact-list contact-list__opacity-titles">
+              <dt>Birth Year:</dt>
+              <dd>1991</dd>
+              <dt>Phone:</dt>
+              <dd><a href="tel:+821099419961">010-9941-9961</a></dd>
+              <dt>Email:</dt>
+              <dd><a href="mailto:heykkyu@gmail.com">heykkyu@gmail.com</a></dd>
+              <dt>Address:</dt>
+              <dd>Gangnam-gu, Seoul, South Korea</dd>
+            </dl>
+          </div>
+          <p class="personal-profile__social">
+            <a href="https://previews.aspirity.com/spirit/" target="_blank"><i class="fa fa-github"></i></a>
+            <a href="https://previews.aspirity.com/spirit/" target="_blank"><i class="fa fa-linkedin-square"></i></a>
+            <a href="https://previews.aspirity.com/spirit/" target="_blank"><i class="fa fa-facebook-square"></i></a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </header>
+<!--Header-->
   </div>
 </template>
 
@@ -42,18 +47,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
