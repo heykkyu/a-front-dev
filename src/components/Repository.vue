6d0fe357bf29e83c11b28b016d5ca955d274c4cf
heykkyu <heykkyu@gmail.com>
2020-04-01 23:59:16 +0900

    axios

---
+++ src/components/Repository.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="section-comp">
+    {{this.list}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+  export default {
+    mounted() {
+      this.get();
+    },
+    data() {
+      return {
+        list: []
+      }
+    },
+    methods: {
+      get() {
+        axios.get('//api.github.com/user/repos', 
+        // {headers: "Authorization: 'token 06e50a5aed2c45f3434eac3082d050e819998ca7'"})
+         {
+            headers: { Authorization: "token 06e50a5aed2c45f3434eac3082d050e819998ca7"}
+        })
+        .then(res => {
+          console.log(res);
+          this.list = res;
+        })
+
+      }
+    }
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="Resume">
     <Resume />
     <Intro />
+    <Repository />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import Resume from '@/components/Resume.vue'
 import Intro from '@/components/Intro.vue'
+import Repository from '@/components/Repository.vue'
 
 export default {
   name: 'home',
   components: {
     Resume,
-    Intro
+    Intro,
+    Repository
   }
 }
 </script>
