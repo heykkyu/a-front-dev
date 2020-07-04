76f9b047cf42f8bb09c028b54220531a8cc2ae00
heykkyu <heykkyu@gmail.com>
2020-07-04 23:07:53 +0900

    added dev note

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,19 @@
 <template>
+  <div class="header">
     <div class="menu">
       <div class="container">
-      <div class="row">
-        <div class="menu__wrapper d-none d-lg-block col-md-12">
-          <nav class="">
-            <ul>
-              <router-link to="#hello">HELLO</router-link>
-              <router-link to="#profile">PROFILE</router-link>
-              <router-link to="#project">PROJECT</router-link>
-              <router-link to="#repository">REPOSITORY</router-link>
-            </ul>
-          </nav>
+        <div class="row">
+          <div class="menu__wrapper d-none d-lg-block col-md-12">
+            <nav class="">
+              <ul>
+                <router-link to="#hello">HELLO</router-link>
+                <router-link to="#profile">PROFILE</router-link>
+                <router-link to="#project">PROJECT</router-link>
+                <router-link to="#repository">REPOSITORY</router-link>
+              </ul>
+            </nav>
+          </div>
         </div>
-        <!-- <div class="menu__wrapper col-md-12 d-lg-none">
-          <button type="button" class="menu__mobile-button">
-            <li class="mobile-lines">
-              <span></span>
-              <span></span>
-              <span></span>
-            </li>
-          </button>
-        </div> -->
       </div>
     </div>
     <div class="mobile-menu d-lg-none">
@@ -38,120 +31,136 @@
         </nav>
       </div>
     </div>
+    <div class="dev-page">
+      <a href="//www.notion.so/e7e4b5b832f64d5fb831b8925b272a21" target="_blank">
+        DEV NOTE(KOR)
+      </a>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.menu {
-  position: fixed;
-  background: rgba(0,0,0, 0.2);
-  padding: 0;
-  height: 0;
-  top: 0;
-  z-index: 100;
-  width: 100%;
-  height: 70px;
-  margin-bottom: 100px;
-  transition: all .5s;
-  color: #fff;
-  &.active {
-    background-color: #fff;
-    box-shadow: 0 5px 8px 0 rgba(0,0,0,.14);
+.header {
+  .dev-page {
+    a {
+      position: fixed;
+      top: 3%;
+      right: 5%;
+      z-index: 9999;
+      font-weight: bold;
+    }
   }
-  .menu__wrapper {
-    z-index: 1;
-    margin-top: 30px;
-    transition: all .3s;
-    nav {
-      font-size: 14px;
-      line-height: 20px;
-      text-transform: uppercase;
-      font-weight: 700;
-      ul {
-        padding-left: 0;
-        a {
-          padding-right: 50px;
+  .menu {
+    position: fixed;
+    background: rgba(0,0,0, 0.2);
+    padding: 0;
+    height: 0;
+    top: 0;
+    z-index: 100;
+    width: 100%;
+    height: 70px;
+    margin-bottom: 100px;
+    transition: all .5s;
+    color: #fff;
+    &.active {
+      background-color: #fff;
+      box-shadow: 0 5px 8px 0 rgba(0,0,0,.14);
+    }
+    .menu__wrapper {
+      z-index: 1;
+      margin-top: 30px;
+      transition: all .3s;
+      nav {
+        font-size: 14px;
+        line-height: 20px;
+        text-transform: uppercase;
+        font-weight: 700;
+        ul {
+          padding-left: 0;
+          a {
+            padding-right: 50px;
+          }
+        }
+      }
+      button {
+        background: none;
+        border: none;
+      }
+      .mobile-lines {
+        display: block;
+        position: relative;
+        width: 30px;
+        height: 25px;
+        text-align: right;
+        margin: 3px;
+        span {
+          display: inline-block;
+          -webkit-transition: all .4s;
+          transition: all .4s;
+          -webkit-box-sizing: border-box;
+          box-sizing: border-box;
+          position: relative;
+          left: 0;
+          width: 100%;
+          height: 4px;
+          background-color: #1f75fe;
+          border-radius: 4px;
         }
       }
     }
-    button {
-      background: none;
-      border: none;
+    @media screen and (max-width: 990px) {
+      display: none;
     }
-    .mobile-lines {
-      display: block;
-      position: relative;
-      width: 30px;
-      height: 25px;
-      text-align: right;
-      margin: 3px;
-      span {
-        display: inline-block;
-        -webkit-transition: all .4s;
-        transition: all .4s;
-        -webkit-box-sizing: border-box;
-        box-sizing: border-box;
-        position: relative;
-        left: 0;
-        width: 100%;
-        height: 4px;
-        background-color: #1f75fe;
-        border-radius: 4px;
+  }
+  .mobile-menu {
+    position: fixed;
+    top: 20px;
+    left: 0;
+    width: 0;
+    height: 0;
+    overflow: hidden;
+    text-align: center;
+    z-index: 9999;
+    transition: all .3s ease;
+    padding: 0;
+    background-color: rgba(0,0,0,.8);
+    opacity: 0;
+    &.active {
+      height: 100%;
+      width: 100%;
+      top: 0;
+      opacity: 1;
+    }
+    .container {
+      margin-right: auto;
+      margin-left: auto;
+      padding-right: 15px;
+      padding-left: 15px;
+      width: 100%;
+      .mobile-menu__close {
+        color: #fff;
+        transition: all .3s;
+        position: absolute;
+        top: 50px;
+        background-color: transparent;
+        border: none;
       }
-    }
-  }
-  @media screen and (max-width: 990px) {
-    display: none;
-  }
-}
-.mobile-menu {
-  position: fixed;
-  top: 20px;
-  left: 0;
-  width: 0;
-  height: 0;
-  overflow: hidden;
-  text-align: center;
-  z-index: 9999;
-  transition: all .3s ease;
-  padding: 0;
-  background-color: rgba(0,0,0,.8);
-  opacity: 0;
-  &.active {
-    height: 100%;
-    width: 100%;
-    top: 0;
-    opacity: 1;
-  }
-  .container {
-    margin-right: auto;
-    margin-left: auto;
-    padding-right: 15px;
-    padding-left: 15px;
-    width: 100%;
-    .mobile-menu__close {
-      color: #fff;
-      transition: all .3s;
-      position: absolute;
-      top: 50px;
-      background-color: transparent;
-      border: none;
-    }
-    .mobile-menu__wrapper {
-      padding: 30px 0;
-      margin: 0;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      min-height: 100%;
-      li {
-        list-style: none;
-        padding: 6px 0;
-        a {
-          color: #fff;
-          display: inline-block;
-          padding: 11px;
-          position: relative;
+      .mobile-menu__wrapper {
+        padding: 30px 0;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        min-height: 100%;
+        li {
+          list-style: none;
+          padding: 6px 0;
+          a {
+            color: #fff;
+            display: inline-block;
+            padding: 11px;
+            position: relative;
+          }
         }
       }
     }
